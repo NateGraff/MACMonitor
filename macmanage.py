ecfe81d85fd3d87f69c9d75a5d89972067db02f4
Nathaniel Graff <nathaniel.graff@gmail.com>
2015-09-15 18:22:47 -0700

    Database creation and dump

--- macmanage.py
+++ macmanage.py
@@ -1,8 +1,65 @@
 #!/usr/bin/env python
 import sqlite3
+import os
+
+DATABASE_NAME = "macmonitor.db"
+
+def create_database():
+	print("Creating MAC Monitor Sqlite3 Database")
+
+	conn = sqlite3.connect(DATABASE_NAME)
+	c = conn.cursor()
+	c.execute('PRAGMA foreign_keys = ON')
+	c.execute('''
+		CREATE TABLE devices
+			(id INTEGER PRIMARY KEY,
+			 mac TEXT)
+		''')
+	c.execute('''
+		CREATE TABLE connections
+			(device INTEGER,
+			 date INTEGER,
+			 ip TEXT,
+			 open INTEGER DEFAULT 1,
+			 FOREIGN KEY(device) REFERENCES devices(id))
+		''')
+	conn.close()
+
+def delete_database():
+	print("Deleting MAC Monitor Sqlite3 Database")
+	os.remove(DATABASE_NAME)
+
+def dump_database():
+	conn = sqlite3.connect(DATABASE_NAME)
+	c = conn.cursor()
+	c.execute('''
+		SELECT mac, date, ip, open
+		FROM devices, connections
+		WHERE devices.id = connections.device
+		''')
+	print(c.fetchall())
+	conn.close()
+
+def insert_example_data():
+	conn = sqlite3.connect(DATABASE_NAME)
+	c = conn.cursor()
+	c.execute('''
+		INSERT INTO devices(mac)
+		VALUES ("foo")
+		''')
+	c.execute('''
+		INSERT INTO connections(device, date, ip)
+		VALUES (1, 123, "bar")
+		''')
+	conn.commit()
+	conn.close()
 
 if __name__ == "__main__":
 	print("Running MAC Manage")
+	delete_database()
+	create_database()
+	insert_example_data()
+	dump_database()
 	# provide options to
 	#   - Print statistics
 	#   - clean/initialize database
